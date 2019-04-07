72d08f590e2c0e7c1bfdf6cdcd2a61a3146e2dca
alexisbatistabustavino <Alexis05batista@gmail.com>
2019-04-07 14:37:21 -0500

    config de ui

--- RestauranteUI/main.py
+++ RestauranteUI/main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__, static_folder="restaurantes/build/static", template_folder="restaurantes/build")
+
+
 @app.route("/")
 def hello():
     return render_template('index.html')
-print('Starting Flask!')
-app.debug=True
-app.run(host='0.0.0.0', port=5001, debug=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080,debug=True)
