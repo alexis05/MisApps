83ee32beb62460b78ff8f0eedcea8d05dda16081
alexisbatistabustavino <Alexis05batista@gmail.com>
2019-09-08 22:11:38 -0500

    Se remueve el proyecto de sitepa

--- sitepa/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-python manage.py makemigrations registros
-python manage.py migrate
-#python manage.py collectstatic
-#python manage.py createsuperuser
-python manage.py runserver 0:8000
