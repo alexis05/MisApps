eb00b2900c495f9cf512e3ea8e08ec42ee117b66
alexisbatistabustavino <alexis05batista@gmail.com>
2020-03-31 23:02:17 -0500

    Se eliminan proyectos que ya no se utilizan

--- RestauranteUI/restaurantes/src/Reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import productosReducer from "./listaDeProductosReducer";
-
-export default combineReducers({
-  productosReducer
-});
--- TurismoUI/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__, static_folder="turismo/build/static", template_folder="turismo/build"))
-
-@app.route("/")
-def hello():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 8080, debug = True)
