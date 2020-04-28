9d9a3d84ddf7e51b5d16e0d314c29dc82dbe70c4
alexisbatistabustavino <alexis05batista@gmail.com>
2020-04-27 23:14:59 -0500

    Se suben cambios para usar WP en BO

---
+++ RestauranteUI/webpack.bo.dev.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "tienda-admin/src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "distbo"),
+    filename: "distbo/js/[name].js",
+    publicPath: "http://localhost:3001/",
+    chunkFilename: "distbo/js/[id].[chunkhash].js",
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, "distbo"),
+    open: true,
+    port: 3001,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              [
+                "@babel/plugin-proposal-class-properties",
+                {
+                  loose: true,
+                },
+                "@babel/plugin-syntax-dynamic-import",
+              ],
+              "@babel/plugin-transform-runtime",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "assets/",
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "tienda-admin/public/index.html"),
+    }),
+  ],
+};
